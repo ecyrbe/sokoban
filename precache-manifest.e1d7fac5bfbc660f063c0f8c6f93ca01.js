336719c693d69620af857a8c75aa75445ed9beff
Alfredo MOREIRA-ROSA <alfredo.moreira-rosa-ext@socgen.com>
2020-04-19 15:45:20 +0200

    Updates

---
+++ precache-manifest.e1d7fac5bfbc660f063c0f8c6f93ca01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbad1a66ae25f69cb8b7f6990af84c75",
+    "url": "/sokoban/index.html"
+  },
+  {
+    "revision": "569ae04147ea8afc9ed5",
+    "url": "/sokoban/static/css/main.4c9a82b5.chunk.css"
+  },
+  {
+    "revision": "04ea456ace41c4dbe012",
+    "url": "/sokoban/static/js/2.6dbd2f9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sokoban/static/js/2.6dbd2f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569ae04147ea8afc9ed5",
+    "url": "/sokoban/static/js/main.83067931.chunk.js"
+  },
+  {
+    "revision": "9ff72358fffb95db9e7e",
+    "url": "/sokoban/static/js/runtime-main.79158c23.js"
+  }
+]);
